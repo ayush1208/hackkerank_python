60aa106189d6363e771baf52b23e11021e0d0d86
peguin40 <ang.sweechow@dhs.sg>
2017-01-26 10:07:28 +0800

    Create py_if_else.py

---
+++ py_if_else.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    if n%2==0:
+        if 6<=n<=20:
+            print("Weird")
+        else:
+            print("Not Weird")
+    else:
+        print("Weird")
+    
